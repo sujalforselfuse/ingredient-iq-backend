003ea014225a150e2b47811e227a74d0ebfc4038
sujal <sujalforselfuse@gmail.com>
2024-05-30 21:37:05 +0530

    llm

--- routes/prompt.js
+++ routes/prompt.js
@@ -1,7 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyDyM3kEhL8nQMRLH2PZmC9HsZ64zVPPQNA");
 
 async function run() {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
